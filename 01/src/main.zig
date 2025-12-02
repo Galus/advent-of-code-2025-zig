dd63e5c4c7dc261a038f81d1ce57c4a027054797
Galus <mgalus@gmail.com>
2025-12-01 20:04:32 -0500

    Day 1 part 1

--- 01/src/main.zig
+++ 01/src/main.zig
@@ -1,10 +1,48 @@
 const std = @import("std");
 const _01 = @import("_01");
 
+const Allocator = std.mem.Allocator;
+
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
     try _01.bufferedPrint();
+
+    var dial: i32 = 50;
+    std.debug.print("dial: {d}.\n", .{dial});
+
+    //const allocator = std.heap.page_allocator;
+    const file_name = "input";
+
+    const file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+
+    const max_bytes = 10 * 1024 * 1024;
+    var buffer: [max_bytes]u8 = undefined;
+    var file_reader: std.fs.File.Reader = file.reader(&buffer);
+    // const ioreader: *std.Io.Reader = &file_reader.interface;
+    var password: usize = 0;
+    while (file_reader.interface.takeDelimiterExclusive('\n')) |string| {
+        std.debug.print("{s}\n", .{string});
+        const direction = string[0];
+        const rotation = try std.fmt.parseInt(i32, string[1..], 10);
+        std.debug.print("{c}\n", .{direction});
+        if (direction == 'R') {
+            std.debug.print("Dialing right {d}\n", .{rotation});
+            dial = @mod((dial + rotation + 1000), 100);
+        } else {
+            std.debug.print("Dialing left {d}\n", .{rotation});
+            dial = @mod((dial - rotation + 1000), 100);
+        }
+        std.debug.print("Dial: {d}\n", .{dial});
+        if (dial == 0) {
+            password = password + 1;
+        }
+    } else |err| {
+        std.debug.print("An error occured: {any}", .{err});
+    }
+
+    std.debug.print("Password: {d}\n", .{password});
 }
 
 test "simple test" {
